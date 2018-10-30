240b14353b787e5f7017a8d87c0a3147fc3698fb
Javier Alvarez <j.f-alvarez@outlook.com>
2018-10-30 17:01:44 -0300

    30102018

---
+++ WebDriverDemo2/WebDriverDemo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+
+namespace WebDriverDemo2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (IWebDriver driver = new FirefoxDriver(@"C:\Users\Simtlix\Desktop\Drivers"))
+            {
+                driver.Navigate().GoToUrl("http://www.google.com");
+                var searchTextBox = driver.FindElement(By.ClassName("gsfi"));
+                searchTextBox.SendKeys("pluralsight");
+                var googleSearchButton = driver.FindElement(By.ClassName("lsb"));
+                googleSearchButton.Click();
+                var imageSearchButton = driver.FindElement(By.XPath("/html/body/div[6]/div[3]/div[5]/div/div/div[1]/div/div/div[1]/div/div[3]/a"));
+                imageSearchButton.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                var searchResults = driver.FindElements(By.XPath("//*[@id='rg_s']/div"));
+
+                searchResults[0].Click();
+            }
+            //driver.Close();
+
+
+
+        }
+
+
+        
+    }
+}
